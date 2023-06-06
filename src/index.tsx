bf255565c7a728271fc4904baa6120a43799869b
Phuong Anh <anh.ngphuong@kyanon.digital>
2023-06-06 11:00:08 +0700

    feature/config-antd-react-router
    - config react router, antd
    - install lodash

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Routers from './routers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Routers />
   </React.StrictMode>
 );
 
